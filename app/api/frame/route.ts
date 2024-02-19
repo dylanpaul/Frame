47af2cee8fc74c8e4c9a7feb3850b3f13a908108
dylanpaul <dhp3190@gmail.com>
2024-02-19 01:44:43 -0500

    A lot of changes

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,10 +1,17 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
+import NFT from '../../../constants/NFT.json';
+import { privateKeyToAccount } from 'viem/accounts';
+import { baseSepolia } from 'viem/chains';
+import { createWalletClient, http, createPublicClient } from 'viem';
+require('dotenv').config();
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const PROVIDER_URL = process.env.PROVIDER_URL;
+const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
-  let text: string | undefined = '';
 
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
@@ -13,17 +20,84 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
+  const nftOwnerAccount = privateKeyToAccount(WALLET_PRIVATE_KEY as `0x${string}`);
+  const nftOwnerClient = createWalletClient({
+    account: nftOwnerAccount,
+    chain: baseSepolia,
+    transport: http(PROVIDER_URL as string),
+  });
+
+  const publicClient = createPublicClient({
+    chain: baseSepolia,
+    transport: http(PROVIDER_URL as string),
+  });
+
+  let minted = false;
+
+  try {
+    minted = !!(await publicClient.readContract({
+      address: NFT_CONTRACT_ADDRESS as `0x${string}`,
+      abi: NFT.abi,
+      functionName: 'minted',
+      args: [accountAddress],
+    }));
+  } catch (err) {
+    console.error(err);
+  }
+
+  if (minted) {
+    return new NextResponse(
+      getFrameHtmlResponse({
+        buttons: [
+          {
+            label: 'You already minted, thanks!',
+          },
+        ],
+        image: {
+          src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
+        },
+      }),
+    );
+  } else {
+    // Try to mint and airdrop the NFT
+    try {
+      const { request } = await publicClient.simulateContract({
+        account: nftOwnerAccount,
+        address: NFT_CONTRACT_ADDRESS as `0x${string}`,
+        abi: NFT.abi,
+        functionName: 'mintFor',
+        args: [accountAddress],
+      });
+      await nftOwnerClient.writeContract(request);
+    } catch (err) {
+      console.error(err);
+    }
+    if (minted) {
+      return new NextResponse(
+        getFrameHtmlResponse({
+          buttons: [
+            {
+              label: 'Thanks for minting!',
+            },
+          ],
+          image: {
+            src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
+          },
+        }),
+      );
+    }
+  }
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `NFT sent to: ${accountAddress}`,
+          label: `Minting Failed`,
         },
       ],
       image: {
         src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
       },
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+      post_url: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
   );
 }
