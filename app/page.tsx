3179c6a002ea2ea6d2a54c548d6800a8358eedcd
dylanpaul <dhp3190@gmail.com>
2024-02-19 00:41:02 -0500

    other stuff

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
     aspectRatio: '1:1',
   },
   input: {
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'zizzamia.xyz',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    images: [`${NEXT_PUBLIC_URL}/GoldStar.jpeg`],
   },
   other: {
     ...frameMetadata,
