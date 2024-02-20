66f8eb488cfdec59d351405824da2b478ffbf287
dylanpaul <dhp3190@gmail.com>
2024-02-19 23:42:59 -0500

    commenting out payment attempt

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -4,56 +4,56 @@ import { NEXT_PUBLIC_URL } from '../../config';
 import NFT from '../../../constants/NFT.json';
 import { privateKeyToAccount } from 'viem/accounts';
 import { baseSepolia } from 'viem/chains';
-import { MetaMaskInpageProvider } from "@metamask/providers";
+// import { MetaMaskInpageProvider } from "@metamask/providers";
 import { createWalletClient, http, createPublicClient } from 'viem';
 require('dotenv').config();
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 const PROVIDER_URL = process.env.PROVIDER_URL;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
-const ITEM_PRICE_IN_WEI = 10 * 1e18; // Assuming the price is $10
+// const ITEM_PRICE_IN_WEI = 10 * 1e18; // Assuming the price is $10
 
-declare global {
-  interface Window{
-    ethereum?:MetaMaskInpageProvider
-  }
-}
+// declare global {
+//   interface Window{
+//     ethereum?:MetaMaskInpageProvider
+//   }
+// }
 
-async function checkMetaMaskConnection(targetAccount: string): Promise<boolean> {
-  if (window.ethereum && window.ethereum.isMetaMask) {
-    try {
-      // Requesting accounts to check if the user is connected
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+// async function checkMetaMaskConnection(targetAccount: string): Promise<boolean> {
+//   if (window.ethereum && window.ethereum.isMetaMask) {
+//     try {
+//       // Requesting accounts to check if the user is connected
+//       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-      if (Array.isArray(accounts)  && accounts.length > 0) {
-        // Check if the connected account matches the target account
-        return accounts[0].toLowerCase() === targetAccount.toLowerCase();
-      } else {
-        return false;
-      }
-    } catch (error) {
-      console.error('Error connecting to MetaMask:', error);
-      return false;
-    }
-  } else {
-    console.log('MetaMask not installed');
-    return false;
-  }
-}
+//       if (Array.isArray(accounts)  && accounts.length > 0) {
+//         // Check if the connected account matches the target account
+//         return accounts[0].toLowerCase() === targetAccount.toLowerCase();
+//       } else {
+//         return false;
+//       }
+//     } catch (error) {
+//       console.error('Error connecting to MetaMask:', error);
+//       return false;
+//     }
+//   } else {
+//     console.log('MetaMask not installed');
+//     return false;
+//   }
+// }
 
-async function makePaymentRequest(buyerAddress: string, sellerAddress: string, itemPriceInWei: number) {
-  try {
-    // Start wallet payment process
-    const response = await window.ethereum!.request({
-      method: 'eth_sendTransaction',
-      params: [{ from: buyerAddress, to: sellerAddress, value: itemPriceInWei }],
-    });
-    console.log(response);
-    return true;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
+// async function makePaymentRequest(buyerAddress: string, sellerAddress: string, itemPriceInWei: number) {
+//   try {
+//     // Start wallet payment process
+//     const response = await window.ethereum!.request({
+//       method: 'eth_sendTransaction',
+//       params: [{ from: buyerAddress, to: sellerAddress, value: itemPriceInWei }],
+//     });
+//     console.log(response);
+//     return true;
+//   } catch (error) {
+//     console.error(error);
+//     return false;
+//   }
+// }
 
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
@@ -68,24 +68,24 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   }
 
   // Check if MetaMask is connected
-  const isConnected = await checkMetaMaskConnection(accountAddress);
+  // const isConnected = await checkMetaMaskConnection(accountAddress);
 
-  if (!isConnected) {
+  // if (!isConnected) {
     // Prompt the user to connect MetaMask
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: `Connect to specified custody address: Buy again!`,
-          },
-        ],
-        image: {
-          src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
-        },
-        post_url: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-  }
+  //   return new NextResponse(
+  //     getFrameHtmlResponse({
+  //       buttons: [
+  //         {
+  //           label: `Connect to specified custody address: Buy again!`,
+  //         },
+  //       ],
+  //       image: {
+  //         src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
+  //       },
+  //       post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+  //     }),
+  //   );
+  // }
 
   // console.log(accountAddress) address is custody address connected to Farcaster
 
@@ -130,9 +130,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     );
   } else {
     // Prompt for payment before minting
-    const paymentSuccessful = await makePaymentRequest(accountAddress, '0x861Aea40c9AcC62435cEa31b2078FF3e022D6627', ITEM_PRICE_IN_WEI);
+    // const paymentSuccessful = await makePaymentRequest(accountAddress, '0x861Aea40c9AcC62435cEa31b2078FF3e022D6627', ITEM_PRICE_IN_WEI);
 
-    if (paymentSuccessful) {
+    // if (paymentSuccessful) {
     // Try to mint and airdrop the NFT after successful payment
       try {
         const { request } = await publicClient.simulateContract({
@@ -163,12 +163,12 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         );
       }
     }
-  }
+  // }
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `Payment Failed: Try again!`,
+          label: `Mint NFT!`,
         },
       ],
       image: {
