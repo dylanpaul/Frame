526a287fc4be9e36448a1131eeb9195eb3c09788
dylanpaul <dhp3190@gmail.com>
2024-02-25 14:26:43 -0500

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const frameMetadata = getFrameMetadata({
     src: `${NEXT_PUBLIC_URL}/GoldStar.jpeg`,
     aspectRatio: '1:1',
   },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame1`,
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
