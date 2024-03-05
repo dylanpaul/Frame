44caf13492b6987ef803d94ac6dc56bff94cf3c2
dylanpaul <dhp3190@gmail.com>
2024-03-05 00:23:01 -0500

    last try before bed

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -29,7 +29,7 @@ export async function GET(request: Request) {
             justifyContent: 'center',
             flexDirection: 'column',
             backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
-            // fontSize: 80,
+            fontSize: 20,
             fontWeight: 700,
             textAlign: 'center',
           }}
